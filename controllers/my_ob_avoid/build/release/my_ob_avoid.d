74de68e02614bfca1e16b7535de4888727084353
husnain77 <61380335+husnain77@users.noreply.github.com>
2023-05-07 19:51:24 +0500

    Added Controller
    Added Controller for obstacle avoidance

---
+++ controllers/my_ob_avoid/build/release/my_ob_avoid.d
@@ -0,0 +1,6 @@
+build/release/my_ob_avoid.o: my_ob_avoid.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp
