93b9aa87225712f85ca7116538ba36bea51d583d
husnain77 <61380335+husnain77@users.noreply.github.com>
2023-05-16 23:40:13 +0500

    Obstacle Avoidance code added to Controller
    Added Obstacle Avoidance technique and wrote code in python to implement obstacle avoidance.

--- controllers/PY_script_1/PY_script_1.py
+++ controllers/PY_script_1/PY_script_1.py
@@ -24,4 +24,56 @@ rl_motor.setPosition(float('inf'))
 # Set initial velocity
 velocity = 1.0
 
+# Loop over the simulation time steps
+while robot.step(64) != -1:
+    print("Left touch sensor value:", ds_left.getValue())
+    print("Right touch sensor value:", ds_right.getValue())
+    # Check if either touch sensor has been triggered
+    if ds_left.getValue() > 0 or ds_right.getValue() > 0:
+        # If either touch sensor is triggered, stop the robot and turn it around
+        fl_motor.setVelocity(-velocity)
+        fr_motor.setVelocity(-velocity)
+        rl_motor.setVelocity(-velocity)
+        rr_motor.setVelocity(-velocity)
+        robot.step(1000)  # wait for the robot to stop moving
+        fl_motor.setVelocity(-velocity*2)
+        fr_motor.setVelocity(velocity/2)
+        rl_motor.setVelocity(-velocity*2)
+        rr_motor.setVelocity(velocity/2)
+        robot.step(1000)  # wait for the robot to turn around
+    else:
+        # Otherwise, move the robot forward
+        fl_motor.setVelocity(velocity)
+        fr_motor.setVelocity(velocity)
+        rl_motor.setVelocity(velocity)
+        rr_motor.setVelocity(velocity)
 
+    # Check if the robot is too close to an obstacle based on touch sensors
+    # Here we assume that a touch sensor is triggered when its value is 1 (meaning contact with an obstacle)
+    if ds_left.getValue() > 0 and ds_right.getValue() > 0:
+        # If both touch sensors are triggered (i.e., the robot has collided with an obstacle),
+        # stop the robot and turn it around
+        fl_motor.setVelocity(-velocity)
+        fr_motor.setVelocity(-velocity)
+        rl_motor.setVelocity(-velocity)
+        rr_motor.setVelocity(-velocity)
+        robot.step(1000)  # wait for the robot to stop moving
+        fl_motor.setVelocity(-velocity)
+        fr_motor.setVelocity(velocity)
+        rl_motor.setVelocity(-velocity)
+        rr_motor.setVelocity(velocity)
+        robot.step(1000)  # wait for the robot to turn around
+    elif ds_left.getValue() > 0:
+        # If only the left touch sensor is triggered, turn right to avoid the obstacle
+        fl_motor.setVelocity(velocity)
+        fr_motor.setVelocity(-velocity)
+        rl_motor.setVelocity(velocity)
+        rr_motor.setVelocity(-velocity)
+        robot.step(1000)  # wait for the robot to turn
+    elif ds_right.getValue() > 0:
+        # If only the right touch sensor is triggered, turn left to avoid the obstacle
+        fl_motor.setVelocity(-velocity)
+        fr_motor.setVelocity(velocity)
+        rl_motor.setVelocity(-velocity)
+        rr_motor.setVelocity(velocity)
+        robot.step(1000)  # wait for the robot to turn
